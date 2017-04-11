dad487bba12b36e75a5ca7f13468f47a83600071
13661892653 <470563152@qq.com>
2017-04-11 16:16:07 +0800

    y

--- pyCode/jkxy/06/controlDB.py
+++ pyCode/jkxy/06/controlDB.py
@@ -1,13 +1,16 @@
 #coding=utf-8
-import pymongo
-connection=pymongo.MongoClient()
-tdb=connection.Jikexueyuan
-post_info=tdb['test']
+from pymongo import MongoClient
+client =MongoClient()
+db=client.Jikexueyuan
+coll=db.primer
 Jike={'name':'极客','age':'5','skill':'Python'}
 god={'name':'玉皇大帝','age':'36','skill':'creatanything','other':'王母娘娘不是他的老婆'}
 godslaver={'name':'月老','age':'unKnown','other':'他的老婆叫如来！'}
-post_info.save(Jike)
-#post_info.save(god)
-#post_info.save(godslaver)
+#coll.insert_one(Jike)
+#coll.insert_one(god)
+#coll.insert_one(godslaver)
 #post_info.remove({'name':u'极客'})
 print('操作数据完成')
+for item in coll.find():
+        print(item)
+
