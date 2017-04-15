8eb739cb0411e6729e089a7587c955883f4e596c
13661892653 <470563152@qq.com>
2017-04-15 23:40:12 +0800

    scrapy

--- pyCode/novel/novel/spiders/novelspider.py
+++ pyCode/novel/novel/spiders/novelspider.py
@@ -5,43 +5,53 @@ Py:Python3.5
 Author:colby_chen
 Date:2017-04-13
 '''
+from scrapy import Request
 from scrapy.spiders import CrawlSpider
-from scrapy.selector import Selector
+from scrapy.selector import Selector, HtmlXPathSelector
 from novel.items import NovelItem
 class novSpider(CrawlSpider):
     name="novelspider"
     redis_key="novelspider:start_urls"
     start_urls=['http://www.daomubiji.com']
+
+    def parse_item(self, response):
+        sell = Selector(response)
+        sites = sell.xpath('/html/body/section/div[2]/div/article/a/text()').extract()
+        print('sites',sites)
+        item={}
+        for site in sites:
+            print(site)
+            item['zj']=site
+            print(item)
+            yield item
     def parse(self,response):
         #print('response',response)
         selector=Selector(response)
         #print('selector',selector.extract())
         #table=selector.xpath('//table')
-        article = selector.xpath('//*[@class="article-content"]//a')
-        print('xxxxxxxxxxxxxxxxxxtable',article)
-        print('xxxxxxxxxxxxxxxxxxtable',article.extract())
+        article = selector.xpath('//article/p/a')
+        #print('xxxxxxxxxxxxxxxxxxtable',article)
+        #print('xxxxxxxxxxxxxxxxxxtable',article.extract())
+        items=[]
         for each in article:
-            print('each',each)
-            bookName=each.xpath('a/div/h2/text()').extract()
-            print('bookName',bookName)
-            content=each.xpath('a/div/p/text()').extract()
-            print('content',content)
-            url=each.xpath('a/@href').extract()
-            print('url',url)
-            for i in range(len(url)):
-                print('i',i)
-                item=NovelItem()
-                item['bookName']=bookName
-                item['chapterURL']=url[i]
-                '''try:
-                    item['bookTitle']=content[i].split(' ')[0]
-                    item['chapterNum'] = content[i].split(' ')[1]
-                except Exception as e:
-                    continue
-                try:
-                    item['chapterName'] = content[i].split(' ')[2]
-                except Exception as e:
-                    item['chapterName'] = content[i].split(' ')[1][-3:]'''
-                print('item',item)
-                yield item
+            #print('each',each)
+            url = each.xpath('@href').extract()[0]
+            items.append(url)
+        print(items)
+        for item in items:
+            yield Request(item, callback=self.parse_item)
+            #bookName=each.xpath('div/h2/text()').extract()
+            #print('bookName',bookName)
+            #content=each.xpath('div/p/text()').extract()
+            #print('content',content)
 
+        '''
+        print('url',url)
+        for i in range(len(url)):
+            print('i',i)
+            item=NovelItem()
+            item['bookName']=bookName
+            item['chapterURL']=url[i]
+            print('item',item)
+            #yield item
+        '''
