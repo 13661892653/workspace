ddda3dbe3dde3505d1cd175796daea06bf278fc7
13661892653 <470563152@qq.com>
2017-05-04 10:34:57 +0800

    u

--- pyCode/craw/demo.py
+++ pyCode/craw/demo.py
@@ -1,20 +1,9 @@
 #coding=utf-8
 """抓取百度首页的html代码"""
 import urllib.request
-import re
-req=urllib.request.Request("http://www.baidu.com")
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+url="http://www.qingtaoke.com/qingsou?cat=12&s_type=1&sort=1"
+req = urllib.request.Request(url=url, headers=headers)
 response = urllib.request.urlopen(req)
-#print(response.read())
-s='chen,zhang,li,zhao,bi'
-pa=r'^chen'
-pa1=r'bi$'
-pa2=r'[abc]'
-pa3=r'zha[on]'
-print(re.findall(pa,s))
-print(re.findall(pa1,s))
-print(re.findall(pa2,s))
-print(re.findall(pa3,s))
-phone='0817-3447210'
-#pa4=r'[0-9]{3,4}-[0-9]{7,8}'
-pa5=r'^[0-9]*-[0-9]*$'
-print(re.findall(pa5,phone))
+page=response.read()
+print(page)
