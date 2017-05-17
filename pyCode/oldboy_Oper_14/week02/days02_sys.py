359e5aca89f2cf2fe74b600f9c476f569067e913
13661892653 <470563152@qq.com>
2017-05-17 18:37:47 +0800

    t

--- pyCode/oldboy_Oper_14/week02/days02_sys.py
+++ pyCode/oldboy_Oper_14/week02/days02_sys.py
@@ -3,7 +3,7 @@
 #Author:Lun.Chen
 #Version:Python3.6
 #Tools :Pycharm 2017.1.2
-
+'''
 import sys
 
 print(sys.path)  # 打印环境变量
@@ -15,3 +15,6 @@ import os
 os.system("dir")  # 执行命令不保存结果
 print(os.popen("dir").read())  # 执行命令后可以保存结果
 #os.mkdir("test_dir")  # 创建目录
+'''
+name=['bb','bb','cc','dd']
+print(name.index('bb'))
