2ed9083d284809f08e3993d6533546dbd0ad7046
13661892653 <470563152@qq.com>
2017-06-20 16:26:53 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/oldboy_Oper_14/week03/coding.py
+++ pyCode/oldboy_Oper_14/week03/coding.py
@@ -3,7 +3,6 @@
 #Author:Lun.Chen
 #Version:Python3.6
 #Tools :Pycharm 2017.1.2
-
 def f(x,l=[]):
     for i in range(x):
         l.append(i*i)
@@ -11,3 +10,12 @@ def f(x,l=[]):
 f(2)
 f(3,[3,2,1])
 f(3)
+def test1(*args,**kargs):
+    pass
+class test2():
+    def self(x,y):
+        self.x = x
+        self.y = y
+    def function(self):
+        print(self.x)
+        print(self.y)
