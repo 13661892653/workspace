77071268d6a9b168af759215ccab9c23bdb41aa9
13661892653 <470563152@qq.com>
2017-07-24 23:18:53 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/test/微信手工保存图片以保存日期命名.py
+++ pyCode/test/微信手工保存图片以保存日期命名.py
@@ -57,5 +57,5 @@ def getImageName(dir):
             file3=startFormatDate(file2)
         rm(dir,fileNameSource_file,file3,fileNameSource_houzui)
 if __name__=="__main__":
-    dir="F:\BaiduNetdiskDownload\WeiXin"
+    dir="F:\\照片\\20170526_儿子照片\\照片"
     getImageName(dir)
