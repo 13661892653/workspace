630fc73a65605a39ef1050f023055671f46c6907
13661892653 <470563152@qq.com>
2017-09-01 17:52:45 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/oldboy_Oper_14/笔记/Python基础笔记16.py
+++ pyCode/oldboy_Oper_14/笔记/Python基础笔记16.py
@@ -92,8 +92,73 @@ DOM学习：
 	}
                函数方法命名都是驼峰命名，如className,第一个单词首字母小写，之后其他单词首字母大写
 	var obj=document.getElementById('id1').classList
+	CSS样式操作
 	var obj_style=obj.style.      
                 
+	属性操作：
+		getAttribute 获取属性值
+		setAttribute 设置属性值
+		obj.setAttribute('属性名','属性值')
+		obj.setAttribute('属性名','属性值')
+	创建标签
+		tag.createElement('标签名称')
+		a.字符串形式
+		b.对象的方式
+		tag.createElement('dvi') #创建div标签
+		
+	提交表单：
+	<script>
+		function submitForm(){
+			document.getElementById('form1').submit()
+		}
+	</script>
+	
+	其他操作
+		confirm:确认框，返回True或者False
+		location.href获取当前页面的url
+		location.href=""#设置当前url
+		location.href=reload #刷新页面
+		si=setInterval(function(){},5000)
+		clearInterval(si)
+		
+		si=setTimeout(function(){},5000)
+		clearTimeout(si)
+	
+	事件
+		onclick 点击
+		onblur 光标移开
+		onfocus 光标插进去
+		绑定事件两种方式
+			a.直接标签绑定
+			b.先获取Dom对象，然后进行绑定
+			document.getElementById('xx').onclick
+			document.getElementById('xx').onfocus
+		this 当前触发事件的标签
+			a.第一种绑定方式
+			<input type="button" onclick="clickOn(this)">
+			function clickOn(self){
+				//self 当前点击的标签
+			}
+			b.第二种绑定方式
+			<input id="i1" type=button>
+			document.getElementById('i1').onclick=function(){
+				//this代指当前点击的标签
+			}
+			c.第三种绑定方式
+			
+		//作用域的示例
+		var myTrs =document.getElementByTagName('tr');
+		var len=myTrs.length;
+		for(var i=0;i<len;i++){
+			myTrs[i].onmouseover=function(){
+				#错误写法 myTrs[i].style.backgoundColor='red'
+				this.style.backgoundColor='red'
+			}
+		}
+		
+		作用域-词法分析
+			分析完之后，再赋值。
+		
 	
 	
 	
