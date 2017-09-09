234bc9e8d1a9a2aa9f76eebf42296b0445609547
13661892653 <470563152@qq.com>
2017-09-09 11:24:51 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/DjangoProject/eltMonitor/etl/views.py
+++ pyCode/DjangoProject/eltMonitor/etl/views.py
@@ -6,12 +6,14 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 def index(request):
-    username=request.POST.get('username')
-    password = request.POST.get('password')
-    if username=='colby' and password=='123':
-        return render(request,'login.html')
-    else:
-        msg='用户名或者密码错误'
+    msg=""
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password = request.POST.get('password')
+        if username=='colby' and password=='123':
+            return render(request,'login.html')
+        else:
+            msg='用户名或者密码错误'
     return render(request,'index.html',{'msg':msg})
 
 def login(request):
