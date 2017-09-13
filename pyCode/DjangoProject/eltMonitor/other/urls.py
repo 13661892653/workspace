b790bbe018eccea4308db5db36335cce6ae2148d
13661892653 <470563152@qq.com>
2017-09-13 23:32:32 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ pyCode/DjangoProject/eltMonitor/other/urls.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+#Version:python3.5.2
+#Tools:Pycharm 2017.1
+__author__ = 'colby'
+from django.conf.urls import url,include
+from other import views
+urlpatterns = [
+    url(r'^index/', views.index),
+]
