e2357dd7fddd8ba81958934e6e0e6b7d87165653
13661892653 <470563152@qq.com>
2017-10-01 21:41:23 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/DjangoProject/etlMonitor/etl/views.py
+++ pyCode/DjangoProject/etlMonitor/etl/views.py
@@ -10,30 +10,45 @@ from etl import models
 from comm import paging
 
 def index(request):
-    if request.method == 'GET':
-        return render(request, 'index.html')
-    elif request.method == 'POST':
-        job_seq_id = request.POST.get('job_seq_id')
-        job_name = request.POST.get('job_name')
-        job_type = request.POST.get('job_type')
-        job_status = request.POST.getlist('job_status')[0]
-        data_prd_st = request.POST.get('data_prd_st')
-        data_prd_ed = request.POST.get('data_prd_ed')
-        if data_prd_st == '':
-            data_prd_st = '0000-01-01'
-        if data_prd_ed == '':
-            data_prd_ed = '2099-01-01'
+    pageCodeNum = 10
+    totalCount = 1000
+    currentPage = request.GET.get('page', 1)
+    perSize = request.COOKIES.get("per_page_count", 20)
+    print('111,perSize', perSize)
+    perSize = int(perSize)
+    print('222,perSize', perSize)
+    currentPage = int(currentPage)
 
-        obj = models.JOB_LOG.objects.filter(JOB_SEQ_ID__contains=job_seq_id,
-                                            JOB_NM__contains=job_name,
-                                            # JOB_TYPE=job_type,
-                                            JOB_STS__contains=job_status,
-                                            DATA_PRD__gt=data_prd_st,
-                                            DATA_PRD__lt=data_prd_ed
-                                            )
-        record = obj.count()
-        # print('obj',obj,'record',record)
-        return render(request, 'index.html', {'data': obj, 'record': record})
+    job_seq_id = request.POST.get('job_seq_id')
+    job_name = request.POST.get('job_name')
+    print(job_seq_id,job_name)
+    job_type = request.POST.get('job_type')
+    print(request.POST.getlist('job_status'))
+    job_status = request.POST.getlist('job_status')
+    data_prd_st = request.POST.get('data_prd_st')
+    data_prd_ed = request.POST.get('data_prd_ed')
+    if data_prd_st == '':
+        data_prd_st = '0000-01-01'
+    if data_prd_ed == '':
+        data_prd_ed = '2099-01-01'
+    totalCount = models.JOB_LOG.objects.filter(JOB_SEQ_ID__contains=job_seq_id,
+                                        JOB_NM__contains=job_name,
+                                        # JOB_TYPE=job_type,
+                                        JOB_STS__contains=job_status,
+                                        DATA_PRD__gt=data_prd_st,
+                                        DATA_PRD__lt=data_prd_ed
+                                        ).count()
+    pageObj = paging.Page(currentPage, totalCount, perSize, pageCodeNum)
+    data = models.JOB_LOG.objects.filter(JOB_SEQ_ID__contains=job_seq_id,
+                                        JOB_NM__contains=job_name,
+                                        # JOB_TYPE=job_type,
+                                        JOB_STS__contains=job_status,
+                                        DATA_PRD__gt=data_prd_st,
+                                        DATA_PRD__lt=data_prd_ed
+                                        )[pageObj.beginCount:pageObj.endCount]
+    pageStr = pageObj.pageStr("/etl/index/")
+    # print('obj',obj,'record',record)
+    return render(request, 'index.html', {'data': data, 'record': totalCount,'pageStr': pageStr})
 
 
 def detail_delete(request, nid):
---
+++ pyCode/oldboy_Oper_14/week22/testList.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+#Version:python3.5.2
+#Tools:Pycharm 2017.1
+__author__ = 'colby'
+lst=[1,2,3,4,5,6]
+print(lst[0:100])
