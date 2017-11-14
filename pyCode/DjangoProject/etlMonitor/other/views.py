623913454e43abc11805e91dc90dc9b274d9ed04
13661892653 <470563152@qq.com>
2017-11-14 19:04:09 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/DjangoProject/etlMonitor/other/views.py
+++ pyCode/DjangoProject/etlMonitor/other/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
 # Create your views here.
-def index(requets):
+def index(requests):
     HttpResponse('hello idea...')
