bd983b00b9b87718fc49808485fcbb97b31172f6
13661892653 <470563152@qq.com>
2017-12-26 18:03:51 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/MxOnline/MxOnline/settings.py
+++ pyCode/MxOnline/MxOnline/settings.py
@@ -15,7 +15,7 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
-
+#sys.path.insert(0,os.path.join(BASE_DIR,'extra_apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'organization',
     'operation',
     'xadmin',
+    'crispy_forms'
 ]
 AUTH_USER_MODEL="users.UserProfile"
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'MxOnline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
