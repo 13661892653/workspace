91f5e942ac386f8445bc956996813e4ab79d88cd
13661892653 <470563152@qq.com>
2018-01-01 19:32:54 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/MxOnline/MxOnline/urls.py
+++ pyCode/MxOnline/MxOnline/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
+from django.views.generic import TemplateView
 import xadmin
 urlpatterns = [
     url('xadmin/', xadmin.site.urls),
+    url('^$',TemplateView.as_view(template_name="index.html"),name="index")
 ]
