6ab35788fd42bdec988886e7460fee697b24f35d
13661892653 <470563152@qq.com>
2018-03-19 18:06:50 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/Python3.6网络爬虫实战/分析Ajax请求并抓取今日头条街拍美图.py
+++ pyCode/Python3.6网络爬虫实战/分析Ajax请求并抓取今日头条街拍美图.py
@@ -3,8 +3,15 @@ import requests
 from bs4 import BeautifulSoup
 import pymongo
 import re
+def get_page_index():
+    pass
+
+def parse_one_page():
+    pass
+
+def get_many_page():
+    pass
 
 
-
-if __name__=="__mani__":
+if __name__=="__main__":
     url="https://www.toutiao.com/"
