ef84617e14925380712b06a2a9e5c05512352f6a
13661892653 <470563152@qq.com>
2018-03-26 15:22:41 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/python3.6网络爬虫实战/16-使用Selenium模拟浏览器抓取淘宝商品美食信息/16-使用Selenium模拟浏览器抓取淘宝商品美食信息.py
+++ pyCode/python3.6网络爬虫实战/16-使用Selenium模拟浏览器抓取淘宝商品美食信息/16-使用Selenium模拟浏览器抓取淘宝商品美食信息.py
@@ -5,17 +5,22 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pyquery as pq
+from pyquery import PyQuery
 import pymongo
 import re
+from config import *
+
 browser = webdriver.Chrome()
+client=pymongo.MongoClient(MONGO_URL)
+db=client[MONGO_DB]
+
 def search():
     try:
         browser.get('https://www.taobao.com/')
         input = WebDriverWait(browser, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "#q")))
         submit=WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
-        input.send_keys('美食')
+        input.send_keys('python视频')
         submit.click()
         total=WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,'#mainsrp-pager > div > div > div > div.total')))
         get_products()
@@ -49,19 +54,26 @@ def get_products():
         EC.presence_of_element_located((By.CSS_SELECTOR,'#mainsrp-itemlist .items .item'))
     )
     html=browser.page_source
-    doc=pq(html)
+    doc=PyQuery(html)
     items=doc('#mainsrp-itemlist .items .item').items()
     for item in items:
         product={
-            'image': item.find('.pic .img').attr('src'),
-            'price': item.find('.price').text(),
-            'deal': item.find('.deal-cnt').text(),
-            'title': item.find('.title').text(),
-            'shop': item.find('.shop').text(),
-            'location': item.find('.location').text()
+            '首图': item.find('.pic .img').attr('src'),
+            '价格': item.find('.price').text().replace('\n',' '),
+            '购买人数': item.find('.deal-cnt').text()[0:-3],
+            '宝贝标题': item.find('.title').text().replace('\n',' '),
+            '店铺名称': item.find('.shop').text(),
+            '店铺位置': item.find('.location').text()
         }
         print(product)
+        save_to_mongo(product)
 
+def save_to_mongo(result):
+    if result:
+        if db[MONGO_TABLE].insert(result):
+            print('插入成功')
+            return True
+        return False
 
 
 def main():
