960e5f8b310f0dd09d7cabccfd147d529a01b9d2
13661892653 <470563152@qq.com>
2018-03-26 10:16:00 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/Python3.6网络爬虫实战/16-使用Selenium模拟浏览器抓取淘宝商品美食信息/16-使用Selenium模拟浏览器抓取淘宝商品美食信息.py
+++ pyCode/Python3.6网络爬虫实战/16-使用Selenium模拟浏览器抓取淘宝商品美食信息/16-使用Selenium模拟浏览器抓取淘宝商品美食信息.py
@@ -12,14 +12,16 @@ browser = webdriver.Chrome()
 def search():
     try:
         browser.get('https://www.taobao.com/')
-        input = WebDriverWait(browser, 10).until(
+        input = WebDriverWait(browser, 50).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "#q")))
-        submit=WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
-        input.send_keys('python视频')
+        submit=WebDriverWait(browser,50).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
+        input.send_keys('美食')
         submit.click()
-        total=WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,'#mainsrp-pager > div > div > div > div.total')))
+        total=WebDriverWait(browser, 50).until(EC.presence_of_element_located((By.CSS_SELECTOR,'#mainsrp-pager > div > div > div > div.total')))
+        get_products()
         return total.text
     except TimeoutException:
+        print('超时啦.............',TimeoutException)
         return search()
 
 
@@ -35,17 +37,29 @@ def next_page(page_number):
         WebDriverWait(browser, 10).until(
             EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > ul > li.item.active > span'),str(page_number))
         )
+        get_products()
     except TimeoutException:
+        print('超时啦.............')
         next_page(page_number)
-def get_product():
-    WebDriverWait(browser, 10).until(
+
+
+def get_products():
+    WebDriverWait(browser, 50).until(
         EC.presence_of_element_located((By.CSS_SELECTOR,'#mainsrp-itemlist .items item'))
     )
     html=browser.page_source
     doc=pyquery(html)
-    items=doc('#mainsrp-itemlist .items item').items()
+    items=doc('#mainsrp-itemlist .items .item').items()
     for item in items:
-        print(item)
+        product={
+            'image': item.find('.pic .img').attr('src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text(),
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
+        }
+        print(product)
 
 
 
