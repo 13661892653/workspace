913b8bac93170654b465496c170fdff56ca204b6
13661892653 <470563152@qq.com>
2018-04-11 17:58:00 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/MongoToMysql/operateMongoDB.py
+++ pyCode/MongoToMysql/operateMongoDB.py
@@ -1,34 +1,65 @@
 #coding=utf-8
 import pymongo
+class MongoDB(object):
+    def __init__(self,user,password,server,port):
+        """
+        mongodb=MongoDB('user','password','server','port')
+        :param user:
+        :param password:
+        :param server:
+        :param port:
+        """
+        self.user = user
+        self.password = password
+        self.server = server
+        self.port = port
+        self.url = 'mongodb://' + self.user + ':' + self.password + '@' + self.server + ':' + self.port
+        self.client = pymongo.MongoClient(self.url)
+    def DatabaseNames(self):
+        database_names = self.client.database_names()
+        return database_names
+    def CollectionNames(self):
+        collection_names = self.client.collection_names()
+        return collection_names
+
 def ConnectMongoDB():
     user=''
     pwd=''
     server='localhost'
     port='27017'
-    dbName='toutiao'
+    dbName='jikexueyuan'
     url = 'mongodb://' + user + ':' + pwd + '@' + server + ':' + port + '/' + dbName
     #client = pymongo.MongoClient(url)
     client=pymongo.MongoClient('localhost',27017)
-    db=client['toutiao']
-    data=db['toutiao'].find()
-    for i in data:
-        print(list(i.keys()))
-        return list(i.keys())
+    return client
 
-def FormatDDL(column,tableName='toutiao'):
+
+def FormatDDL(column,tableName):
     lst=[]
-    lst.append('create table %s(' %tableName)
+    lst.append('''drop table if exists `%s`;''' % tableName)
+    lst.append('''create table `%s`(''' %tableName)
     for col in column:
-        lst.append('%s varchar(50) comment '',' %col)
-    lst.append('primary key(_id)'
-               ')')
+        length=' '*int(50-col.__len__())
+        #保证生成的DDL对齐，距离字段名称最左边50个字符对齐。
+        lst.append('''\t`%s`%svarchar(50) comment '',''' %(col,length))
+    lst.append('''\tprimary key(_id)
+    );''')
     return lst
 
 def main():
-    column=ConnectMongoDB()
-    print('column',type(column))
-    ddlStr=FormatDDL(column)
-    for i in ddlStr:
-        print(i)
+    client=ConnectMongoDB()
+    database_names=client.database_names()
+    for database_name in database_names:
+        db=client[database_name]
+        collection_names=db.collection_names()
+        for collection_name in collection_names:
+            data=db[collection_name].find()
+            for content in data:
+                column=list(content.keys())
+                ddlStr = FormatDDL(column,collection_name)
+                for ddl in ddlStr:
+                    print(ddl)
+                break
+
 if __name__=="__main__":
     main()
