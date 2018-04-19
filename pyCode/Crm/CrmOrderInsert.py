39dd7a38a3e010f35a2a1c8e3ec95b5ddc3bd134
13661892653 <470563152@qq.com>
2018-04-19 17:59:23 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/Crm/CrmOrderInsert.py
+++ pyCode/Crm/CrmOrderInsert.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
-
 from urllib.parse import urljoin
 import time
 data={
