1e7201963b27691c84450f285a55e67a7aadeaac
13661892653 <470563152@qq.com>
2018-04-19 23:03:44 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/Crm/CrmOrderInsert.py
+++ pyCode/Crm/CrmOrderInsert.py
@@ -72,7 +72,7 @@ def insert_content(browser,enterprise_name,order_date):
         merchandiser.select_by_visible_text('章科铭')
 
         button = browser.find_element_by_id('btn_addord')
-        button.click()
+        #button.click()
     except Exception as e:
         print('企业订单录入错误：',enterprise_name)
 def left_url(url):
@@ -88,9 +88,9 @@ def main():
     browser=webdriver.Chrome()
     browser.get(url)
     username=browser.find_element_by_id('username')
-    username.send_keys('***')
+    username.send_keys('13486108876')
     password=browser.find_element_by_id('password')
-    password.send_keys('***')
+    password.send_keys('qwe12345')
     button=browser.find_element_by_class_name('btn-success')
     button.click()
 
