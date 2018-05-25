5d5f1a4aa8df907f51e32977412145d38604def3
13661892653 <470563152@qq.com>
2018-05-25 18:01:44 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/logger/src/com/doit/log/LoggerWriter.java
+++ javaCode/logger/src/com/doit/log/LoggerWriter.java
@@ -7,10 +7,10 @@ public class LoggerWriter {
 
 		while (true) {
 			Logger logger = Logger.getLogger("logRollingFile");
-			logger.info("视频类目----"+ System.currentTimeMillis());
+			logger.error("视频类目----"+ System.currentTimeMillis());
 			logger.info("蔬菜类目----"+ System.currentTimeMillis());
 			logger.info("水果类目----"+ System.currentTimeMillis());
-			logger.info("智能家电----"+ System.currentTimeMillis());
+			logger.error("智能家电----"+ System.currentTimeMillis());
 			logger.info("交通工具----"+ System.currentTimeMillis());
 			Thread.sleep(50);
 		}
