def0cac1ab64627d2353e39aa10e73631fe8acc0
13661892653 <470563152@qq.com>
2018-05-28 22:30:43 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/HadoopClient/HdfsClientDemo.py
+++ pyCode/HadoopClient/HdfsClientDemo.py
@@ -2,8 +2,20 @@
 from hdfs import *
 
 def main():
-    client = InsecureClient("http://BIGDATA:50070",user="root")
-    client.upload("/", "F:/BaiduNetdiskDownload/eclipse-SDK-4.6-win32-x86_64.zip",n_threads=2)
+    client = Client("http://COLBY-NN-101:50070",timeout=1000)
+    #upload(client)
+    cat_hdfs_file(client)
 
+
+def upload(client):
+    res_upload=client.upload("/", "F:/BaiduNetdiskDownload/Adobe Acrobat DC Pro v17.009.20058.7z", overwrite=True,n_threads=1)
+    res_list =client.list("/")
+    print(res_upload,res_list)
+
+def cat_hdfs_file(client):
+    f=client.read('/logs/2018-05-27-17/access_log_243d2263-fc00-4255-a562-235469d17ef5.log',encoding='gbk')
+    with f as reader:
+        print(reader.read())
 if __name__ == "__main__":
     main()
+
