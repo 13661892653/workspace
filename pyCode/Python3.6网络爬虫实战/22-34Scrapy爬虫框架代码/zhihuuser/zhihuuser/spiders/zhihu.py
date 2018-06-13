fee63159e8f47bf71b877a2d76ccddcd08525621
13661892653 <470563152@qq.com>
2018-06-13 22:58:59 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- pyCode/Python3.6网络爬虫实战/22-34Scrapy爬虫框架代码/zhihuuser/zhihuuser/spiders/zhihu.py
+++ pyCode/Python3.6网络爬虫实战/22-34Scrapy爬虫框架代码/zhihuuser/zhihuuser/spiders/zhihu.py
@@ -10,15 +10,20 @@ class ZhihuSpider(Spider):
     name = 'zhihu'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
+    start_user='excited-vczh'
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query = 'allow_message%2Cis_followed%2Cis_following%2Cis_org%2Cis_blocking%2Cemployments%2Canswer_count%2Cfollower_count%2Carticles_count%2Cgender%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics'
+    follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
+    follows_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
 
     def start_requests(self):
-        url='https://www.zhihu.com/api/v4/members/hao-hao-32-13?include=allow_message%2Cis_followed%2Cis_following%2Cis_org%2Cis_blocking%2Cemployments%2Canswer_count%2Cfollower_count%2Carticles_count%2Cgender%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics'
-        follows_url='https://www.zhihu.com/api/v4/members/excited-vczh/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=20&limit=20'
-        yield Request(url=url,callback=self.parse_follows)
+        yield Request(self.user_url.format(user=self.start_user,include=self.user_query),self.parse_user)
+        yield Request(self.follows_url.format(user=self.start_user, include=self.user_query,offset=0,limit=20), self.parse_follows)
 
-    def parse_follows(self, response):
+
+    def parse_user(self, response):
         print(response.text)
 
 
-    def parse_followers(self, response):
-        pass
+    def parse_follows(self, response):
+        print(response.text)
