b93716fe0f89ec780dc59bc3f4a6c59116615b20
13661892653 <470563152@qq.com>
2018-07-07 22:42:06 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/com/easyrong/data/days10/AbstractSonDemo.java
@@ -0,0 +1,17 @@
+package com.easyrong.data.days10;
+
+public  class AbstractSonDemo extends AbstractDemo{
+    /*
+     *
+     */
+
+    @Override
+    public void work() {
+        System.out.println("重写work方法");
+    }
+
+    @Override
+    public void action() {
+        System.out.println("重写action方法");
+    }
+}
