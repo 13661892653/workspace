3b4f12af353fef22f36dfcdb0c6b0f07e50c6753
13661892653 <470563152@qq.com>
2018-07-08 23:32:55 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/com/easyrong/data/days11/task/KeyBoard.java
@@ -0,0 +1,17 @@
+package com.easyrong.data.days11.task;
+
+public class KeyBoard implements USB{
+    /*
+     *
+     */
+
+    @Override
+    public void startUSB() {
+        System.out.println("键盘打开USB！");
+    }
+
+    @Override
+    public void closeUSB() {
+        System.out.println("键盘打开USB！");
+    }
+}
