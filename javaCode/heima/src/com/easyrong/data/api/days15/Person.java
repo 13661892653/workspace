9efebf0365c11e7f4ef5971d0c97a902c4919699
13661892653 <470563152@qq.com>
2018-07-12 18:05:07 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/heima/src/com/easyrong/data/api/days15/Person.java
+++ javaCode/heima/src/com/easyrong/data/api/days15/Person.java
@@ -41,5 +41,5 @@ public class Person {
 	public void setAge(int age) {
 		this.age = age;
 	}
-
+	
 }
--- javaCode/heima/src/com/easyrong/data/api/days15/StringDemo.java
+++ javaCode/heima/src/com/easyrong/data/api/days15/StringDemo.java
@@ -8,14 +8,14 @@ public class StringDemo {
 	public static void main(String[] args) {
 		
 		String str="colby";
-		System.out.print("字符串长度："+str.length()+str.);
-		System.out.print("字符串长度："+str.toUpperCase());
-		
-		
-		
-		
+		System.out.println("字符串长度："+str.length()+" "+str.hashCode());
+		System.out.println("字符串长度："+str.toUpperCase());
+		byte bytes[]= {65,66,67,68,69};
+		//offset数组的起始位置
+		String s1=new String(bytes);
+		String s2=new String(bytes,0,5);
+		System.out.println("字符串输出："+s1+"\n字符串截取："+s2);	
 		
 	}
-	
-	
+		
 }
