b05a88ab2dce31509e24fbcdd5875cb7f1d52590
13661892653 <470563152@qq.com>
2018-07-13 17:55:35 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/com/easyrong/data/api/days16/CheckEmaiAddrl.java
@@ -0,0 +1,15 @@
+package com.easyrong.data.api.days16;
+
+public class CheckEmaiAddrl {
+	public static void main(String[] args) {
+		String emailAddr = "colby@126.com";
+		checkMail(emailAddr);
+	}
+
+	public static void checkMail(String emailAddr) {
+		String regex = "[0-9a-zA-Z_]+@[0-9a-zA-Z_]+\\.[0-9a-z_A-Z]+";
+		boolean result = emailAddr.matches(regex);
+		System.out.println(emailAddr + "格式是否合法：" + result);
+	}
+
+}
