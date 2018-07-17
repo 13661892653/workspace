628d0382c4d51ba55c7310928db6b41acc95e241
13661892653 <470563152@qq.com>
2018-07-17 11:22:54 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/heima/src/com/easyrong/data/set/days19/HashDemo.java
+++ javaCode/heima/src/com/easyrong/data/set/days19/HashDemo.java
@@ -1,11 +1,17 @@
 package com.easyrong.data.set.days19;
 
+import java.util.HashSet;
+
 public class HashDemo {
 	public static void main(String[] args) {
-		Person p=new Person();
-		int h=p.hashCode();
-		System.out.println(h);
-		
-		
+		function1();
+	}
+
+	public static void function1() {
+		HashSet<Person> hs = new HashSet<Person>();
+		hs.add(new Person("colby", 3000));
+		hs.add(new Person("colby", 3000));
+		hs.add(new Person("colby", 3000));
+		System.out.println(hs);
 	}
 }
---
+++ javaCode/heima/src/com/easyrong/data/set/days20/MapDemo.java
@@ -0,0 +1,14 @@
+package com.easyrong.data.set.days20;
+/*
+ * Map接口中的常用方法
+ * 使用Map接口的实现类的HashMap
+ */
+public class MapDemo {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void function() {
+		
+	}
+}
