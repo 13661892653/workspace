643d07c35e465dfddb0339daa50d4463b8e74a18
13661892653 <470563152@qq.com>
2018-07-17 17:49:56 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/com/easyrong/data/set/days20/EntrySetDemo.java
@@ -0,0 +1,18 @@
+package com.easyrong.data.set.days20;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class EntrySetDemo {
+	public static void main(String[] args) {
+		Map<String,Integer> map=new HashMap<String,Integer>();
+		map.put("colby", 10);
+		map.put("cl", 20);
+		map.put("pll", 30);
+		map.put("pll", 50);
+		Set<Map.Entry<String, Integer>> set=map.entrySet();
+		System.out.println(set);
+	}
+}
--- javaCode/heima/src/com/easyrong/data/set/days20/MapDemo.java
+++ javaCode/heima/src/com/easyrong/data/set/days20/MapDemo.java
@@ -1,14 +1,29 @@
 package com.easyrong.data.set.days20;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * Map接口中的常用方法
  * 使用Map接口的实现类的HashMap
+ * Map有返回值，插入重复键时返回的是被覆盖之前的值，其他情况返回null值
  */
 public class MapDemo {
 	public static void main(String[] args) {
-		
+		function();
 	}
 	
 	public static void function() {
+		Map<String,Integer> map=new HashMap<String,Integer>();
+		map.put("colby", 10);
+		map.put("cl", 20);
+		map.put("pll", 30);
+		map.put("pll", 50);
+		System.out.println(map.get("pll"));
+		for(String key:map.keySet()) {
+			System.out.println(key+":"+map.get(key));
+		}
 		
 	}
+	
 }
