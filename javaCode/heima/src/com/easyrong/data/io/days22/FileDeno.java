8d8e822c676f0d30c7ff714ae5ac8fc1026aeb47
13661892653 <470563152@qq.com>
2018-07-19 18:16:05 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/com/easyrong/data/io/days22/FileDeno.java
@@ -0,0 +1,26 @@
+package com.easyrong.data.io.days22;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileDeno {
+	public static void main(String[] args) throws IOException {
+		//function01();
+		createDir();
+	}
+	
+	public static void function01() {
+		File file=new File("e:","eclipse");
+		System.out.println(file);
+	}
+	
+	public static void createFile() throws IOException {
+		File file=new File("E:\\project\\workspace\\javaCode\\heima\\src\\com\\easyrong\\data\\io\\days22\\createFile.txt");
+		file.createNewFile();
+	}
+	
+	public static void createDir() {
+		File file=new File("E:\\\\project\\\\workspace\\\\javaCode\\\\heima\\\\src\\\\com\\\\easyrong\\\\data\\\\io\\\\days22\\\\dir");
+		file.mkdirs();
+	}
+}
