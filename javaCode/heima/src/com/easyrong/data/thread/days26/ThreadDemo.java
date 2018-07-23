f273d5c7b57b6f057fd31e442dbdb0c7836f6e9e
13661892653 <470563152@qq.com>
2018-07-23 23:27:35 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/com/easyrong/data/thread/days26/ThreadDemo.java
@@ -0,0 +1,11 @@
+package com.easyrong.data.thread.days26;
+
+public class ThreadDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(1000, 1000);
+		
+		System.out.println("end");
+	}
+
+}
