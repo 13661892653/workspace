47f167cf98e2c32bfbdb6411939bb45b638e015a
13661892653 <470563152@qq.com>
2018-07-24 23:09:18 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/heima/src/com/easyrong/data/thread/days27/RunnableDemo.java
+++ javaCode/heima/src/com/easyrong/data/thread/days27/RunnableDemo.java
@@ -3,11 +3,8 @@ package com.easyrong.data.thread.days27;
 public class RunnableDemo implements Runnable {
 
 	private int tickt = 100;
-	private Object obj = new Object();
-
-	public void run() {
+	public synchronized void run() {
 		while (true) {
-			synchronized (obj) {
 				if (tickt > 0) {
 					try {
 						Thread.sleep(10);
@@ -17,7 +14,6 @@ public class RunnableDemo implements Runnable {
 					}
 					System.out.println(Thread.currentThread().getName() + "正在出售：" + (tickt--));
 				}
-			}
 		}
 	}
 }
