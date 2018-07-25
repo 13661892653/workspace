925a57df81682a4670e3c9fc9367246dc378a409
13661892653 <470563152@qq.com>
2018-07-25 17:45:37 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/heima/src/com/easyrong/data/jdbc/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.easyrong.data.jdbc;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("hello world!");		
-	}
-}
---
+++ javaCode/heima/src/com/easyrong/data/jdbc/days28/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author chenlun
+ *
+ */
+package com.easyrong.data.jdbc.days28;
