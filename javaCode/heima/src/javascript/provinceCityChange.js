4e6ab396915f5bed7728cec193f1c4af3b17f157
13661892653 <470563152@qq.com>
2018-08-02 18:19:43 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heima/src/javascript/provinceCityChange.js
@@ -0,0 +1,7 @@
+function provinceCityChange(){
+    alert("dd");
+    document.write("<script language=javascript src='provinceArr.js'></script>");
+    for(var i=0;i<provinceArr.length;i++){
+        alert(provinceArr[i]);
+    }
+}
