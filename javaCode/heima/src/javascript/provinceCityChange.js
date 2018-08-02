7b0eec057e97c48d1e53793b10ef41a7ff52cfba
13661892653 <470563152@qq.com>
2018-08-02 23:51:39 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/heima/src/javascript/provinceCityChange.js
+++ javaCode/heima/src/javascript/provinceCityChange.js
@@ -1,7 +1,41 @@
 function provinceCityChange(){
-    alert("dd");
-    document.write("<script language=javascript src='provinceArr.js'></script>");
+    var proid=document.getElementById("proid");
     for(var i=0;i<provinceArr.length;i++){
-        alert(provinceArr[i]);
+        var textNode=document.createTextNode(provinceArr[i][0]);
+        //创建一个option元素节点
+        var optionE=document.createElement("option");
+        //将文本节点添加到li元素节点
+        optionE.appendChild(textNode);
+        //再讲li节点添加到ul节点最后
+        proid.appendChild(optionE);
+    }
+}
+
+function getCity(){
+    var tag=document.getElementById("proid");
+    var index=tag.selectedIndex;
+    var proName = tag.options[index].text; // 选中文本
+    console.log(proName);
+    for(var i=0;i<cityArr.length;i++){
+        if(proName==cityArr[i][0]){
+            console.log(cityArr[i][0]);
+            var cityid=document.getElementById("cityId");
+
+            for(var j=0;j<cityArr[i].length;j++){
+                console.log(cityArr[i][j]);
+                /*console.log(cityArr[i][j]);
+                var textNode=document.createTextNode(cityArr[i][j]);
+                //创建一个option元素节点
+                var optionE=document.createElement("option");
+                //将文本节点添加到li元素节点
+                optionE.appendChild(textNode);
+                //再讲li节点添加到ul节点最后
+                cityid.appendChild(optionE);*/
+                var textNode2=document.createTextNode(cityArr[i][j]);
+                var optionE2=document.createElement("option");
+                optionE2.appendChild(textNode2);
+                cityid.appendChild(optionE2)
+            }
+        }
     }
 }
