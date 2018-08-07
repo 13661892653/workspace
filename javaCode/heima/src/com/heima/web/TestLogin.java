3ea100158254a61a6939c39fc28dfeda71823c21
13661892653 <470563152@qq.com>
2018-08-07 10:13:08 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

--- javaCode/heima/src/com/heima/web/TestLogin.java
+++ javaCode/heima/src/com/heima/web/TestLogin.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import org.junit.Test;
 
@@ -15,8 +14,8 @@ public class TestLogin {
 		Connection conn=JDBCUtilsConfig.getConnection();
 		String sql="select * from user where username =? and password = ?";
 		PreparedStatement sta=conn.prepareStatement(sql);
-		sta.setObject(1, "colby");
-		sta.setObject(2, "123456");
+		sta.setObject(1, username);
+		sta.setObject(2, password);
 		ResultSet rs=sta.executeQuery();
 		if(rs.next()){
 			System.out.println("恭喜你，登陆成功-"+rs.getObject("username"));
@@ -30,6 +29,6 @@ public class TestLogin {
 	
 	@Test
 	public void testJunit() throws SQLException {
-		login(null, null);
+		login("colby", "123456");
 	}
 }
