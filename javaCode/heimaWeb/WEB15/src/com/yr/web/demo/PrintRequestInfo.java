7b669439b5978889e49b8ad2436d332390084bd8
13661892653 <470563152@qq.com>
2018-08-13 18:23:16 +0800

    Signed-off-by: 13661892653 <470563152@qq.com>

---
+++ javaCode/heimaWeb/WEB15/src/com/yr/web/demo/PrintRequestInfo.java
@@ -0,0 +1,54 @@
+package com.yr.web.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrintRequestInfo
+ */
+public class PrintRequestInfo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PrintRequestInfo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 获取method
+		String method = request.getMethod();
+		String uri = request.getRequestURI();
+		StringBuffer url = request.getRequestURL();
+		// web应用的名称
+		String context = request.getContextPath(); 
+		// get提交url地址后的参数字符串
+		String queryString = request.getQueryString(); 
+		System.out.println("客户端提交方法：" + method);
+		System.out.println("客户端uri：" + uri);
+		System.out.println("客户端url：" + url);
+		System.out.println("web应用：" + context);
+		System.out.println("查询字符串：" + queryString);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
